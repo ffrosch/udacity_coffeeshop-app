c1f52ff97a459450470e95c67208882bc7049d9f
Florian Frosch <florian.frosch@posteo.de>
2021-02-02 18:35:30 +0100

    chore: automatically reset db on restart

--- backend/src/api.py
+++ backend/src/api.py
@@ -34,8 +34,8 @@ def populate_db():
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
-# populate_db()
+db_drop_and_create_all()
+populate_db()
 
 ## ROUTES
 '''
